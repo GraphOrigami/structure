ab291616a1bb078ae68bce734ed8dcd7ae0844be
Jan Miksovsky <jan@miksovsky.com>
2023-06-08 10:13:01 -0700

    FunctionGraph can wrap a function that takes multiple parameters

--- packages/core/src/FunctionGraph.js
+++ packages/core/src/FunctionGraph.js
@@ -4,18 +4,35 @@
  */
 export default class FunctionGraph {
   /**
-   * @param {Function} fn
-   * @param {any[]} [domain]
+   * @param {function} fn the function to be explored
+   * @param {Iterable<any>} [domain] optional domain of the function
    */
   constructor(fn, domain = []) {
     this.fn = fn;
     this.domain = domain;
   }
 
+  /**
+   * Return the application of the function to the given key.
+   *
+   * @param {any} key
+   */
   async get(key) {
-    return this.fn(key);
+    let value =
+      this.fn.length <= 1
+        ? // Function takes no arguments or only one argument: invoke
+          await this.fn(key)
+        : // Bind the key to the first parameter. Subsequent get calls will
+          // eventually bind all parameters until only one remains. At that point,
+          // the above condition will apply and the function will be invoked.
+          Reflect.construct(this.constructor, [this.fn.bind(this, key)]);
+    return value;
   }
 
+  /**
+   * Enumerates the function's domain (if defined) as the graph's keys. If no domain
+   * was defined, this returns an empty iterator.
+   */
   async keys() {
     return this.domain;
   }
