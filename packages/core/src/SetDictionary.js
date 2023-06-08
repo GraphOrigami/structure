48faa981ecdfecdfe9b6bf1b894a795ee4e96463
Jan Miksovsky <jan@miksovsky.com>
2023-06-08 14:41:33 -0700

    SetDictionary

---
+++ packages/core/src/SetDictionary.js
@@ -0,0 +1,24 @@
+/**
+ * Wraps a Set as an AsyncDictionary.
+ *
+ * This treats the set as an array, useing the integer indexes as keys.
+ *
+ * @typedef {import("@graphorigami/async-dictionary").AsyncDictionary} AsyncDictionary
+ * @implements {AsyncDictionary}
+ */
+export default class SetDictionary {
+  /**
+   * @param {Set} set
+   */
+  constructor(set) {
+    this.values = [...set];
+  }
+
+  async get(key) {
+    return this.values[key];
+  }
+
+  async keys() {
+    return this.values.keys();
+  }
+}
