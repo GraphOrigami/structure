7d40cd927006502344981cc8d2f5e4a442ecb5c9
Jan Miksovsky <jan@miksovsky.com>
2023-06-12 16:09:15 -0700

    Factor FunctionDictionary from FunctionGraph

--- packages/core/src/FunctionGraph.js
+++ packages/core/src/FunctionGraph.js
@@ -1,19 +1,12 @@
+import FunctionDictionary from "./FunctionDictionary.js";
+
 /**
  * A graph defined by a function and an optional domain.
  *
  * @typedef {import("@graphorigami/types").AsyncGraph} AsyncGraph
  * @implements {AsyncGraph}
  */
-export default class FunctionGraph {
-  /**
-   * @param {function} fn the key->value function
-   * @param {Iterable<any>} [domain] optional domain of the function
-   */
-  constructor(fn, domain = []) {
-    this.fn = fn;
-    this.domain = domain;
-  }
-
+export default class FunctionGraph extends FunctionDictionary {
   /**
    * Return the application of the function to the given key.
    *
@@ -23,19 +16,11 @@ export default class FunctionGraph {
     let value =
       this.fn.length <= 1
         ? // Function takes no arguments or only one argument: invoke
-          await this.fn(key)
+          await super.get(key)
         : // Bind the key to the first parameter. Subsequent get calls will
           // eventually bind all parameters until only one remains. At that point,
           // the above condition will apply and the function will be invoked.
           Reflect.construct(this.constructor, [this.fn.bind(this, key)]);
     return value;
-  }
-
-  /**
-   * Enumerates the function's domain (if defined) as the graph's keys. If no domain
-   * was defined, this returns an empty iterator.
-   */
-  async keys() {
-    return this.domain;
   }
 }
